742cd3e11d86d143b58a73c4cd5ecae3f58d2e69
Vivek Kumar Verma <vivk274@gmail.com>
2019-05-17 21:35:02 +0530

    Initial commit

---
+++ src/test/java/com/vivek/cloudconfig/Cloud/Config/Skeleton/CloudConfigSkeletonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vivek.cloudconfig.Cloud.Config.Skeleton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudConfigSkeletonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
